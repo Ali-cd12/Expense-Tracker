1b7ac557081892ef32c27bfcaadc5af4001b0052
Mujtaba <ahmedm.3571@gmail.com>
2025-09-20 11:51:29 +0500

    Created a web app
    An Expense Tracker

---
+++ README.md
@@ -0,0 +1,5 @@
+## An Expense Tracker Web Application
+
+# Keep track of your expenses
+
+A web application created using HTML, CSS and Javascript
---
+++ expenseTracker.js
@@ -0,0 +1,117 @@
+//First Getting elemenst from HTML
+//From the input fileds in the form
+let Expense = document.querySelector("#expense");
+let Price = document.querySelector("#cost");
+
+//Now submit button
+const submit = document.querySelector("#submit");
+
+//Getting Total
+let total = document.querySelector("#total");
+
+//Getting List Section
+let list = document.querySelector("#listSection");
+
+//Getting Lists
+let items = document.querySelectorAll(".cards");
+
+//Now getting delete buttons
+const del = document.querySelectorAll(".delete");
+
+//some variables
+let sum = 0;
+let allExpenses = [ ];
+
+
+//Function Time!!!
+function getsum(){
+    sum = 0;
+    for(exp of allExpenses){
+        sum += exp.value;
+    }
+}
+
+function displayTotal(){
+    total.textContent = `Total: $${sum}`;
+}
+
+function getExpenses(){
+    for(let i = 1; i < items.length; i++){
+        let temp1= items[i].firstElementChild;
+        let temp2 = temp1.nextElementSibling;
+        let edit = temp2.textContent;
+        let cut = edit.indexOf(" ");
+        edit = edit.slice(0, cut);
+        edit = Number(edit);
+        allExpenses.unshift({name: temp1.textContent, value: edit});
+    }
+}
+
+function addExpense(e){
+    e.preventDefault();
+    let word = Expense.value;
+    let price = Price.value;
+    price = Number(price);
+
+    if(isNaN(price)){
+        window.alert("Please Enter a number!");
+    }
+    else{
+        allExpenses.unshift({name: word, value: price});
+        createCard(word, price);
+    }
+}
+
+function createCard(parama, paramb){
+    let pri = document.createElement("p");
+    pri.textContent = parama;
+    pri.setAttribute('class', 'item');
+
+    let dollars = document.createElement("p");
+    dollars.textContent = `${paramb} $`;
+    dollars.setAttribute('class', 'price');
+
+    let but =  document.createElement("button");
+    but.textContent = "DELETE";
+    but.setAttribute('class', 'delete');
+
+    let card = document.createElement("div");
+    card.setAttribute('class' , 'cards');
+    card.append(pri);
+    card.append(dollars);
+    card.append(but);
+    list.insertBefore(card, items[1]);
+    
+    but.addEventListener("click",()=>{
+        let parent = but.parentElement;
+        editarray(parent);
+        parent.remove();
+        items = document.querySelectorAll(".cards");
+    })
+
+    document.getElementById("frm").reset();
+    items = document.querySelectorAll(".cards");
+    update();
+}
+
+function editarray(element){
+    let del = element.firstElementChild;
+    let del1 = del.textContent;
+    for(exp of allExpenses){
+        if(exp.name == del1){
+            allExpenses.splice(allExpenses.indexOf(exp), 1);
+        }
+    }
+    update();
+}
+function update(){
+        getsum();
+        displayTotal();
+}
+function start(){
+    submit.addEventListener("click",addExpense);
+    getExpenses();
+    update();
+}
+
+start();
